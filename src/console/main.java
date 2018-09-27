29284403982439d8d7b0594d6016799668fae201
Natanael Adityasatria <nata.adit@gmail.com>
2018-09-27 15:21:43 +0700

    compare result between algorithms in client and algorithms in server

--- src/console/main.java
+++ src/console/main.java
@@ -8,10 +8,17 @@ import anyplace.AnyUserData;
 import anyplace.AnyUserData.FakeResults;
 import anyplace.LogRecord;
 import anyplace.RadioMap;
+import android.anyplace.*;
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("Start");
+		int algorithm_choice = 1;
+		TestServerAlgorithm(algorithm_choice);
+		TestAndroidAlgorithm(algorithm_choice);
+	}
+
+	private static void TestServerAlgorithm(int algorithm_choice) {
+		System.out.println("Start Server Algorithm");
 
 		String radioMapPath = "C:\\radiomap.txt";
 		File radioMapFile = new File(radioMapPath);
@@ -21,13 +28,33 @@ public class main {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		int algorithm_choice = 1;
 
 		ArrayList<LogRecord> latestScanList = new ArrayList<>();
-		FakeResults r = AnyUserData.fakeScan();
+		FakeResults r = AnyUserData.fakeScan75AyerRajah();
 		latestScanList = r.records;
 
 		String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
+		System.out.println(result);
+		System.out.println("Finish");
+	}
+
+	private static void TestAndroidAlgorithm(int algorithm_choice) {
+		System.out.println("Start Android Algorithm");
+
+		String radioMapPath = "C:\\radiomap.txt";
+		File radioMapFile = new File(radioMapPath);
+		android.anyplace.RadioMap radioMap = null;
+		try {
+			radioMap = new android.anyplace.RadioMap(radioMapFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		ArrayList<LogRecord> latestScanList = new ArrayList<>();
+		FakeResults r = AnyUserData.fakeScan75AyerRajah();
+		latestScanList = r.records;
+
+		String result = android.anyplace.Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
 		System.out.println(result);
 		System.out.println("Finish");
 	}
