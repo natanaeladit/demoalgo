07b3bdf8e1edea8062874969e298fc0d6c770738
Natanael Adityasatria <nata.adit@gmail.com>
2018-10-08 21:32:01 +0800

    add kalman filter

--- src/anyplace/AnyUserData.java
+++ src/anyplace/AnyUserData.java
@@ -3,109 +3,125 @@ package anyplace;
 import java.util.ArrayList;
 
 public class AnyUserData {
-	public static class FakeResults {
-		public ArrayList<LogRecord> records;
-		public int heading;
-	}
+    public static class FakeResults {
+        public ArrayList<LogRecord> records;
+        public int heading;
+    }
 
-	public static FakeResults fakeScan() {
-		return fakeFST01FL2();
-	}
+    public static FakeResults fakeScan() {
+        return fakeFST01FL2();
+    }
 
-	public static FakeResults fakeScan75AyerRajah() {
-		return fake75AyerRajah();
-	}
+    public static FakeResults fakeScan75AyerRajah() {
+        return fake75AyerRajah();
+    }
 
-	private static FakeResults fakeFST01FL2() {
-		FakeResults r = new FakeResults();
-		ArrayList<LogRecord> latestScanList = new ArrayList<LogRecord>();
+    public static FakeResults fakeScanSimple() {
+        return fakeSimple();
+    }
 
-		latestScanList.add(new LogRecord("64:70:02:b5:0c:a2", -66));
-		latestScanList.add(new LogRecord("18:64:72:20:ce:61", -87));
+    private static FakeResults fakeSimple() {
+        FakeResults r = new FakeResults();
+        ArrayList<LogRecord> latestScanList = new ArrayList<LogRecord>();
 
-		r.records = latestScanList;
-		r.heading = 270;
-		return r;
-	}
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:33", -50));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:32", -50));
 
-	private static FakeResults fake75AyerRajah() {
-		FakeResults r = new FakeResults();
-		ArrayList<LogRecord> latestScanList = new ArrayList<LogRecord>();
+        r.records = latestScanList;
+        r.heading = 0;
+        return r;
+    }
 
-		latestScanList.add(new LogRecord("a8:bd:27:a7:24:33", -58));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:24:32", -58));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:24:31", -58));
-		latestScanList.add(new LogRecord("88:5b:dd:a8:5c:64", -110));
-		latestScanList.add(new LogRecord("f0:5c:19:50:cd:92", -87));
-		latestScanList.add(new LogRecord("f0:5c:19:50:cd:91", -87));
-		latestScanList.add(new LogRecord("f0:5c:19:50:cd:93", -88));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:9d:92", -72));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:9d:91", -71));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:9d:93", -71));
+    private static FakeResults fakeFST01FL2() {
+        FakeResults r = new FakeResults();
+        ArrayList<LogRecord> latestScanList = new ArrayList<LogRecord>();
 
-		latestScanList.add(new LogRecord("f0:5c:19:4f:85:13", -110));
-		latestScanList.add(new LogRecord("f0:5c:19:4f:85:11", -110));
-		latestScanList.add(new LogRecord("f0:79:59:77:02:38", -65));
-		latestScanList.add(new LogRecord("2c:56:dc:82:e4:20", -110));
-		latestScanList.add(new LogRecord("2c:56:dc:82:e4:24", -110));
-		latestScanList.add(new LogRecord("60:45:cb:8f:7b:ac", -110));
-		latestScanList.add(new LogRecord("00:1a:8c:0a:ca:01", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:23:73", -73));
-		latestScanList.add(new LogRecord("26:18:1d:28:28:c3", -110));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:9d:81", -110));
+        latestScanList.add(new LogRecord("64:70:02:b5:0c:a2", -66));
+        latestScanList.add(new LogRecord("18:64:72:20:ce:61", -87));
 
-		latestScanList.add(new LogRecord("a8:bd:27:a7:20:a2", -68));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:20:a1", -68));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:9d:83", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:20:a3", -68));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:9d:82", -110));
-		latestScanList.add(new LogRecord("88:d7:f6:82:38:5c", -110));
-		latestScanList.add(new LogRecord("82:2a:a8:a1:99:04", -110));
-		latestScanList.add(new LogRecord("f0:79:59:77:02:3c", -67));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:20:b3", -71));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:20:b2", -71));
+        r.records = latestScanList;
+        r.heading = 270;
+        return r;
+    }
 
-		latestScanList.add(new LogRecord("64:d1:54:4f:34:51", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:20:b1", -71));
-		latestScanList.add(new LogRecord("18:60:24:37:60:ab", -110));
-		latestScanList.add(new LogRecord("e0:8e:3c:35:97:d5", -110));
-		latestScanList.add(new LogRecord("e0:8e:3c:35:97:d4", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a6:ff:b1", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a6:ff:b2", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a6:ff:b3", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:24:23", -58));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:24:22", -110));
+    private static FakeResults fake75AyerRajah() {
+        FakeResults r = new FakeResults();
+        ArrayList<LogRecord> latestScanList = new ArrayList<LogRecord>();
 
-		latestScanList.add(new LogRecord("a8:bd:27:a7:24:21", -59));
-		latestScanList.add(new LogRecord("18:d6:c7:d8:52:a4", -59));
-		latestScanList.add(new LogRecord("4c:ed:fb:b1:37:b0", -110));
-		latestScanList.add(new LogRecord("18:d6:c7:d8:52:a3", -73));
-		latestScanList.add(new LogRecord("18:d6:c7:d8:52:a2", -110));
-		latestScanList.add(new LogRecord("f4:f2:6d:94:78:c2", -110));
-		latestScanList.add(new LogRecord("4c:ed:fb:b1:37:b4", -110));
-		latestScanList.add(new LogRecord("18:d6:c7:d8:54:af", -60));
-		latestScanList.add(new LogRecord("88:d7:f6:bd:ca:c4", -110));
-		latestScanList.add(new LogRecord("a8:bd:27:a7:23:f3", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:33", -58));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:32", -58));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:31", -58));
+        latestScanList.add(new LogRecord("88:5b:dd:a8:5c:64", -110));
+        latestScanList.add(new LogRecord("f0:5c:19:50:cd:92", -87));
+        latestScanList.add(new LogRecord("f0:5c:19:50:cd:91", -87));
+        latestScanList.add(new LogRecord("f0:5c:19:50:cd:93", -88));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:9d:92", -72));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:9d:91", -71));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:9d:93", -71));
 
-		latestScanList.add(new LogRecord("a0:04:60:69:67:de", -87));
-		latestScanList.add(new LogRecord("8a:15:04:bc:02:af", -110));
-		latestScanList.add(new LogRecord("7c:8b:ca:39:02:b2", -110));
-		latestScanList.add(new LogRecord("d2:6e:0e:af:d1:f7", -110));
-		latestScanList.add(new LogRecord("e0:8e:3c:36:fc:89", -84));
-		latestScanList.add(new LogRecord("00:1a:8c:82:4c:37", -89));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:2b:71", -110));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:2b:73", -110));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:2b:72", -86));
+        latestScanList.add(new LogRecord("f0:5c:19:4f:85:13", -110));
+        latestScanList.add(new LogRecord("f0:5c:19:4f:85:11", -110));
+        latestScanList.add(new LogRecord("f0:79:59:77:02:38", -65));
+        latestScanList.add(new LogRecord("2c:56:dc:82:e4:20", -110));
+        latestScanList.add(new LogRecord("2c:56:dc:82:e4:24", -110));
+        latestScanList.add(new LogRecord("60:45:cb:8f:7b:ac", -110));
+        latestScanList.add(new LogRecord("00:1a:8c:0a:ca:01", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:23:73", -73));
+        latestScanList.add(new LogRecord("26:18:1d:28:28:c3", -110));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:9d:81", -110));
 
-		latestScanList.add(new LogRecord("00:1a:8c:82:4c:38", -90));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:aa:03", -110));
-		latestScanList.add(new LogRecord("94:b4:0f:c7:11:a3", -81));
-		latestScanList.add(new LogRecord("fe:ec:da:8b:fd:8c", -90));
-		latestScanList.add(new LogRecord("6c:72:20:11:b4:1a", -87));
-		latestScanList.add(new LogRecord("f0:5c:19:50:ce:53", -87));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:20:a2", -68));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:20:a1", -68));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:9d:83", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:20:a3", -68));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:9d:82", -110));
+        latestScanList.add(new LogRecord("88:d7:f6:82:38:5c", -110));
+        latestScanList.add(new LogRecord("82:2a:a8:a1:99:04", -110));
+        latestScanList.add(new LogRecord("f0:79:59:77:02:3c", -67));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:20:b3", -71));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:20:b2", -71));
 
-		r.records = latestScanList;
-		r.heading = 270;
-		return r;
-	}
+        latestScanList.add(new LogRecord("64:d1:54:4f:34:51", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:20:b1", -71));
+        latestScanList.add(new LogRecord("18:60:24:37:60:ab", -110));
+        latestScanList.add(new LogRecord("e0:8e:3c:35:97:d5", -110));
+        latestScanList.add(new LogRecord("e0:8e:3c:35:97:d4", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a6:ff:b1", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a6:ff:b2", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a6:ff:b3", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:23", -58));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:22", -110));
+
+        latestScanList.add(new LogRecord("a8:bd:27:a7:24:21", -59));
+        latestScanList.add(new LogRecord("18:d6:c7:d8:52:a4", -59));
+        latestScanList.add(new LogRecord("4c:ed:fb:b1:37:b0", -110));
+        latestScanList.add(new LogRecord("18:d6:c7:d8:52:a3", -73));
+        latestScanList.add(new LogRecord("18:d6:c7:d8:52:a2", -110));
+        latestScanList.add(new LogRecord("f4:f2:6d:94:78:c2", -110));
+        latestScanList.add(new LogRecord("4c:ed:fb:b1:37:b4", -110));
+        latestScanList.add(new LogRecord("18:d6:c7:d8:54:af", -60));
+        latestScanList.add(new LogRecord("88:d7:f6:bd:ca:c4", -110));
+        latestScanList.add(new LogRecord("a8:bd:27:a7:23:f3", -110));
+
+        latestScanList.add(new LogRecord("a0:04:60:69:67:de", -87));
+        latestScanList.add(new LogRecord("8a:15:04:bc:02:af", -110));
+        latestScanList.add(new LogRecord("7c:8b:ca:39:02:b2", -110));
+        latestScanList.add(new LogRecord("d2:6e:0e:af:d1:f7", -110));
+        latestScanList.add(new LogRecord("e0:8e:3c:36:fc:89", -84));
+        latestScanList.add(new LogRecord("00:1a:8c:82:4c:37", -89));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:2b:71", -110));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:2b:73", -110));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:2b:72", -86));
+
+        latestScanList.add(new LogRecord("00:1a:8c:82:4c:38", -90));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:aa:03", -110));
+        latestScanList.add(new LogRecord("94:b4:0f:c7:11:a3", -81));
+        latestScanList.add(new LogRecord("fe:ec:da:8b:fd:8c", -90));
+        latestScanList.add(new LogRecord("6c:72:20:11:b4:1a", -87));
+        latestScanList.add(new LogRecord("f0:5c:19:50:ce:53", -87));
+
+        r.records = latestScanList;
+        r.heading = 270;
+        return r;
+    }
 }
--- src/console/main.java
+++ src/console/main.java
@@ -11,133 +11,231 @@ import anyplace.LogRecord;
 import anyplace.RadioMap;
 
 public class main {
-	public static void main(String[] args) {
-		int algorithm_choice = 4;
-		// TestServerAlgorithm(algorithm_choice);
-		// TestAndroidAlgorithm(algorithm_choice);
-		TestKalmanFilter(algorithm_choice);
-		// TestKalmanFilter();
-	}
+    public static void main(String[] args) {
+        int algorithm_choice = 1;
+        //TestServerAlgorithm(algorithm_choice);
+        //TestAndroidAlgorithm(algorithm_choice);
+        TestKalmanFilter(algorithm_choice);
+        //TestKalmanFilter();
+        //TestKNN();
+        //TestWKNN();
+        //TestMAP();
+        //TestMMSE();
+    }
 
-	private static void TestKalmanFilter(int algorithm_choice) {
-		System.out.println("Start Server Algorithm");
+    private static void TestKNN() {
+        System.out.println("Start KNN Algorithm");
 
-		String radioMapPath = "C:\\radiomap.txt";
-		File radioMapFile = new File(radioMapPath);
-		RadioMap radioMap = null;
-		try {
-			radioMap = new RadioMap(radioMapFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        String radioMapPath = "C:\\radiomap_simple.txt";
+        File radioMapFile = new File(radioMapPath);
+        RadioMap radioMap = null;
+        try {
+            radioMap = new RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		ArrayList<LogRecord> latestScanList = new ArrayList<>();
-		FakeResults r = AnyUserData.fakeScan75AyerRajah();
-		latestScanList = r.records;
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScanSimple();
+        latestScanList = r.records;
 
-		String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
-		System.out.println(result);
-		System.out.println("Finish");
+        String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, 1);
+        System.out.println(result);
+        System.out.println("Finish");
+    }
 
-		String[] temp = result.split(" ");
-		double lat = Double.parseDouble(temp[1]);
-		double lng = Double.parseDouble(temp[0]);
+    private static void TestWKNN() {
+        System.out.println("Start WKNN Algorithm");
 
-		double newLat = 103.78644921565798;
-		double newLng = 1.2973656024776129;
-		System.out.println("Random new location:");
-		System.out.println(Double.toString(newLng) + " " + Double.toString(newLat));
+        String radioMapPath = "C:\\radiomap_simple.txt";
+        File radioMapFile = new File(radioMapPath);
+        RadioMap radioMap = null;
+        try {
+            radioMap = new RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		KalmanFilter kalmanFilter = new KalmanFilter(lat, lng);
-		// kalmanFilter.reset(lat, lng);
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScanSimple();
+        latestScanList = r.records;
 
-		System.out.println("Kalman Filter: ");
-		String pos = kalmanFilter.update(newLat, newLng);
-		System.out.println(pos);
-	}
+        String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, 2);
+        System.out.println(result);
+        System.out.println("Finish");
+    }
 
-	private static void TestKalmanFilter() {
-		double[][] wifis = new double[10][];
+    private static void TestMAP() {
+        System.out.println("Start MAP Algorithm");
 
-		wifis[0] = new double[] { -0.1752, 1.6722 };
-		wifis[1] = new double[] { 0.2784, 3.2999 };
-		wifis[2] = new double[] { 1.8177, 3.2585 };
-		wifis[3] = new double[] { 3.4643, 3.1955 };
-		wifis[4] = new double[] { 3.4026, 5.7257 };
-		wifis[5] = new double[] { 5.9359, 5.6577 };
-		wifis[6] = new double[] { 6.1601, 8.0462 };
-		wifis[7] = new double[] { 8.0159, 7.5123 };
-		wifis[8] = new double[] { 8.7819, 10.0279 };
-		wifis[9] = new double[] { 9.6048, 8.2946 };
+        String radioMapPath = "C:\\radiomap_simple.txt";
+        File radioMapFile = new File(radioMapPath);
+        RadioMap radioMap = null;
+        try {
+            radioMap = new RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		double[][] kfl = new double[10][];
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScanSimple();
+        latestScanList = r.records;
 
-		kfl[0] = new double[] { -0.1752, 1.6722 };
-		kfl[1] = new double[] { 0.2706, 3.2720 };
-		kfl[2] = new double[] { 1.6280, 3.5296 };
-		kfl[3] = new double[] { 3.2086, 3.5331 };
-		kfl[4] = new double[] { 3.8219, 5.0287 };
-		kfl[5] = new double[] { 5.4215, 5.7390 };
-		kfl[6] = new double[] { 6.3998, 7.2287 };
-		kfl[7] = new double[] { 7.7391, 7.8945 };
-		kfl[8] = new double[] { 8.8665, 9.2587 };
-		kfl[9] = new double[] { 9.8840, 9.5473 };
+        String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, 3);
+        System.out.println(result);
+        System.out.println("Finish");
+    }
 
-		KalmanFilter filter = new KalmanFilter(wifis[0][0], wifis[0][1]);
+    private static void TestMMSE(){
+        System.out.println("Start MMSE Algorithm");
 
-		for (int i = 1; i < wifis.length; i++) {
-			double[] wifi = wifis[i];
-			KalmanFilter.Point p = new KalmanFilter.Point(0, 0); // = filter.update(wifi[0], wifi[1]);
+        String radioMapPath = "C:\\radiomap_simple.txt";
+        File radioMapFile = new File(radioMapPath);
+        RadioMap radioMap = null;
+        try {
+            radioMap = new RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			System.out.println(String.format("V%d: %4.3f. %4.3f", i, p.x, p.y));
-			System.out.println(String.format("V%d: %4.3f. %4.3f", i, kfl[i][0], kfl[i][1]));
-			if (Math.abs(p.x - kfl[i][0]) > 0.001 || Math.abs(p.y - kfl[i][1]) > 0.001) {
-				System.out.println(String.format("Error"));
-			}
-		}
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScanSimple();
+        latestScanList = r.records;
 
-		System.out.println("Done");
-	}
+        String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, 4);
+        System.out.println(result);
+        System.out.println("Finish");
+    }
 
-	private static void TestServerAlgorithm(int algorithm_choice) {
-		System.out.println("Start Server Algorithm");
+    private static void TestKalmanFilter(int algorithm_choice) {
+        System.out.println("Start Server Algorithm");
 
-		String radioMapPath = "C:\\radiomap.txt";
-		File radioMapFile = new File(radioMapPath);
-		RadioMap radioMap = null;
-		try {
-			radioMap = new RadioMap(radioMapFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        String radioMapPath = "C:\\radiomap_simple.txt";
+        File radioMapFile = new File(radioMapPath);
+        RadioMap radioMap = null;
+        try {
+            radioMap = new RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		ArrayList<LogRecord> latestScanList = new ArrayList<>();
-		FakeResults r = AnyUserData.fakeScan75AyerRajah();
-		latestScanList = r.records;
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScanSimple();
+        latestScanList = r.records;
 
-		String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
-		System.out.println(result);
-		System.out.println("Finish");
-	}
+        String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
+        System.out.println(result);
+        System.out.println("Finish");
 
-	private static void TestAndroidAlgorithm(int algorithm_choice) {
-		System.out.println("Start Android Algorithm");
+        String[] temp = result.split(" ");
+        double lat = Double.parseDouble(temp[1]);
+        double lng = Double.parseDouble(temp[0]);
 
-		String radioMapPath = "C:\\radiomap.txt";
-		File radioMapFile = new File(radioMapPath);
-		android.anyplace.RadioMap radioMap = null;
-		try {
-			radioMap = new android.anyplace.RadioMap(radioMapFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        double newLat = 1.6;
+        double newLng = 1.6;
+        System.out.println("Random new location:");
+        System.out.println(Double.toString(newLng) + " " + Double.toString(newLat));
 
-		ArrayList<LogRecord> latestScanList = new ArrayList<>();
-		FakeResults r = AnyUserData.fakeScan75AyerRajah();
-		latestScanList = r.records;
+        KalmanFilter kalmanFilter = new KalmanFilter(lat, lng);
+        // kalmanFilter.reset(lat, lng);
 
-		String result = android.anyplace.Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
-		System.out.println(result);
-		System.out.println("Finish");
-	}
+        System.out.println("Kalman Filter: ");
+        String pos = kalmanFilter.update(newLat, newLng);
+        System.out.println(pos);
+
+        newLat = 1.7;
+        newLng = 1.7;
+        System.out.println("Random new location:");
+        System.out.println(Double.toString(newLng) + " " + Double.toString(newLat));
+
+        System.out.println("Kalman Filter: ");
+        pos = kalmanFilter.update(newLat, newLng);
+        System.out.println(pos);
+    }
+
+    private static void TestKalmanFilter() {
+        double[][] wifis = new double[10][];
+
+        wifis[0] = new double[]{-0.1752, 1.6722};
+        wifis[1] = new double[]{0.2784, 3.2999};
+        wifis[2] = new double[]{1.8177, 3.2585};
+        wifis[3] = new double[]{3.4643, 3.1955};
+        wifis[4] = new double[]{3.4026, 5.7257};
+        wifis[5] = new double[]{5.9359, 5.6577};
+        wifis[6] = new double[]{6.1601, 8.0462};
+        wifis[7] = new double[]{8.0159, 7.5123};
+        wifis[8] = new double[]{8.7819, 10.0279};
+        wifis[9] = new double[]{9.6048, 8.2946};
+
+        double[][] kfl = new double[10][];
+
+        kfl[0] = new double[]{-0.1752, 1.6722};
+        kfl[1] = new double[]{0.2706, 3.2720};
+        kfl[2] = new double[]{1.6280, 3.5296};
+        kfl[3] = new double[]{3.2086, 3.5331};
+        kfl[4] = new double[]{3.8219, 5.0287};
+        kfl[5] = new double[]{5.4215, 5.7390};
+        kfl[6] = new double[]{6.3998, 7.2287};
+        kfl[7] = new double[]{7.7391, 7.8945};
+        kfl[8] = new double[]{8.8665, 9.2587};
+        kfl[9] = new double[]{9.8840, 9.5473};
+
+        KalmanFilter filter = new KalmanFilter(wifis[0][0], wifis[0][1]);
+
+        for (int i = 1; i < wifis.length; i++) {
+            double[] wifi = wifis[i];
+            KalmanFilter.Point p = new KalmanFilter.Point(0, 0);
+            filter.update(wifi[0], wifi[1]);
+
+            System.out.println(String.format("V%d: %4.3f. %4.3f", i, p.x, p.y));
+            System.out.println(String.format("V%d: %4.3f. %4.3f", i, kfl[i][0], kfl[i][1]));
+            if (Math.abs(p.x - kfl[i][0]) > 0.001 || Math.abs(p.y - kfl[i][1]) > 0.001) {
+                System.out.println(String.format("Error"));
+            }
+        }
+
+        System.out.println("Done");
+    }
+
+    private static void TestServerAlgorithm(int algorithm_choice) {
+        System.out.println("Start Server Algorithm");
+
+        String radioMapPath = "C:\\radiomap.txt";
+        File radioMapFile = new File(radioMapPath);
+        RadioMap radioMap = null;
+        try {
+            radioMap = new RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScan75AyerRajah();
+        latestScanList = r.records;
+
+        String result = Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
+        System.out.println(result);
+        System.out.println("Finish");
+    }
+
+    private static void TestAndroidAlgorithm(int algorithm_choice) {
+        System.out.println("Start Android Algorithm");
+
+        String radioMapPath = "C:\\radiomap.txt";
+        File radioMapFile = new File(radioMapPath);
+        android.anyplace.RadioMap radioMap = null;
+        try {
+            radioMap = new android.anyplace.RadioMap(radioMapFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<LogRecord> latestScanList = new ArrayList<>();
+        FakeResults r = AnyUserData.fakeScan75AyerRajah();
+        latestScanList = r.records;
+
+        String result = android.anyplace.Algorithms.ProcessingAlgorithms(latestScanList, radioMap, algorithm_choice);
+        System.out.println(result);
+        System.out.println("Finish");
+    }
 }
